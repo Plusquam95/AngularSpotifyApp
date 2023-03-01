7e80d4934e893dbab7d561e68447cc679b79d03d
Plusquam95 <erwinramos17@gmail.com>
2023-03-01 14:46:20 -0600

    pipes generation

--- spotify-clone/src/app/shared/pipe/order-list.pipe.ts
+++ spotify-clone/src/app/shared/pipe/order-list.pipe.ts
@@ -1,12 +1,38 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
+import { ModelTracks } from '@core/models/tracks.models';
 @Pipe({
   name: 'orderList'
 })
 export class OrderListPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(value: Array<any>, args: string | null = null, sort: string = 'asc'): ModelTracks[] {
+    
+    try {
 
+      if(args == null){
+        return value
+      } else{
+
+        const tmplist= value.sort((a,b)=>{
+
+          if (a[args] < b[args]){
+            return -1
+          }
+          else if (a[args] == b[args]){
+            return 0;
+          }
+          else if (a[args] > b[args]){
+            return 1;
+          }
+          return 1;
+
+        })
+        return (sort == 'asc') ? tmplist : tmplist.reverse()
+      }
+    } catch (e) {
+      console.log('algo pasó')
+      return value;
+    }
+
+  }
 }
